59af4197dd61264704b2214b96991a42974b9ebc
Rubén Fernández <rubick@gmail.com>
2017-10-10 17:57:37 +0200

    checkpoint WIP due to change of requirements

--- wires.py
+++ wires.py
@@ -1,34 +1,50 @@
 import time
+import Rpi.GPIO as GPIO
+import sys
+import os from subprocess 
+import Popen
 
 combinations = [[1, 2], [3, 4], [5, 6]]
 gpiosUsed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 wires = len(combinations)
+movie=("/data/movie")
+
 correctCombinations = 0
+GPIO.setmode(GPIO.BOARD)
 
 
 def setInputGpios(gpiolist):
-    # TODO set to input gpiosUsed
-    pass
+    GPIO.setup(gpiolist, GPIO.IN)
 
 
 def checkPairStatus(pair):
-    src = pair.pop()
-    # TODO set SRC GPIO to output and set 1
-    dst = pair.pop()
+    # One wire has one source and one destination. 
+    # The source will be used to send high signal and dst will read 
+    srcPin = pair.pop()
+    dstPin = pair.pop()
+    GPIO.setup(srcPin, GPIO.OUT , initial=GPIO.HIGH)
+    dstInputStatus=GPIO.input(dstPin)
 
-    # Check
-    if dst == 1:
+
+    if dstInputStatus == 1:
         correctCombinations += 1
 
-    # TODO: Set src and dst to input
-    return
+    SetInputGpios([scrPint,dstPin])
 
 
 def playEnd():
-    pass
+    os.system('killall omxplayer.bin')
+    omxc = Popen(['omxplayer', '-b', movie])
+
+def checkReset(gpiosUsed):
+    for pin in gpiosUsed:
+        checkcombinations=gpios
+        
 
 
 def main():
+    setInputGpios(gpiosUsed)
+
     while correctCombinations < wires:
         correctCombinations = 0
         setInputGpios()
@@ -38,6 +54,7 @@ def main():
     time.sleep(0.2)
     playEnd()
     # TODO:Only play once. Wait for a button pressed
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
