bee911ab95284522c1c607978d783071c0bfdc20
Rubén Fernández <rubick@gmail.com>
2017-10-11 23:04:37 +0200

    checkpoint

--- wires.py
+++ wires.py
@@ -5,70 +5,86 @@ import subprocess
 
 class Thing(object):
     instances = []
-    """Class that will handle a button,selector,etc"""
+    """Class that will handle a button,selector, switch, etc"""
 
     def __init__(self, activePort, validPort, *passivePorts):
         self.activePort = activePort
         self.validPort = validPort
         self.passivePorts = passivePorts
         Thing.instances.append(self)
-        inputPorts = list(passivePorts)
-        inputPorts.append(validPort)
 
-        setInputGpios(inputPorts)
-        #TODO: Set output gpios
-        #TODO: change setInputGpios for input/output function
+        # input ports are the ones that could receive voltaje that is valid plus passive ports
+        self.inputPorts = list(passivePorts)
+        self.inputPorts.append(validPort)
 
-    def CheckCompleted(self):
-        pass
+        setGpio(inputPorts, "input")
+        setGpio(activePort, "output")
+
+    def checkCompleted(self):
+        """if we activate the activeport and we receive in the validport, the Thing is solved"""
+        """ We don't store the status in a class variables because the status of each Thing 
+        must be checked each time you want to know 
+        the overall status of the puzzle"""
+
+        GPIO.output(self.activePort, True)
+        # TODO: Change to interrupts
+        if GPIO.input(self.validPort):        
+            return True
+        else:
+            return False
+        GPIO.output(self.activePort, False)
+        
+
+    def checkReset(self):
+       """Test if the Thing is in its normal(reset) state""" 
+        GPIO.output(self.activePort, True)
+        for port in self.inputPorts:
+            if GPIO.input(port):
+                return False
+            else:
+                return True
 
 
-    def CheckReset(self):
-        pass
 
-
-
-class WiresArray(Thing):
+class WiresPuzzle(Thing):
     """handle a wires puzzle composed for various Things"""
+    """This puzzle could be something like a pairs of female-female connectors plus more 
+    connectors dummies (connected as passive ports) and pairs of wires male-male"""
     ##TODO: Eliminate append instance
-    pass
+    def __init__(self, steps):
+        """ steps is a tuple that contain n Things"""
+        self.steps = steps
+
+    def __len__(self):
+        return len(self.steps)
+
+    def checkCompleted(self):
+        """all Things must be completed at the same time """
+        for thing in self.steps:
+            if thing.checkCompleted() == False:
+                return False
+            else:
+                return True
+
 
 class Game:
     """This class will handle the data of the game"""
     pass
 
 
-def setInputGpios(gpiolist):
-    GPIO.setup(gpiolist, GPIO.IN)
+def setGpio(gpiolist, io):
+    """ set a gpio or list of them as input or output mode """
+    if io == "input":
+        GPIO.setup(gpiolist, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    elif io == "output":
+        GPIO.setup(gpiolist, GPIO.OUT , initial=GPIO.LOW)
+
 
 GPIO.setmode(GPIO.BOARD)
-
-combinations = [[1, 2], [3, 4], [5, 6]]
-gpiosUsed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-wires = len(combinations)
-movie=("/data/movie")
-
-
-
-
-
-def checkPairStatus(pair):
-    # One wire has one source and one destination. 
-    # The source will be used to send high signal and dst will read 
-    srcPin = pair.pop()
-    dstPin = pair.pop()
-    GPIO.setup(srcPin, GPIO.OUT , initial=GPIO.HIGH)
-    dstInputStatus=GPIO.input(dstPin)
-
-
-    if dstInputStatus == 1:
-        correctCombinations += 1
-
-    SetInputGpios([scrPint,dstPin])
-
+movie=("/data/precuelaGotham.mov")
 
 def playEnd():
-    subprocess.run('killall omxplayer.bin')
+    subprocess.run(['killall', 'omxplayer.bin'])
     omxc =  subprocess.Popen(['omxplayer', '-b', movie])
 
 def checkReset(gpiosUsed):
@@ -76,18 +92,17 @@ def checkReset(gpiosUsed):
         checkcombinations=gpios
         
 def main():
+    playing = False
+    test=Thing(40,38)
+    while True:
+        time.sleep(0.2)
+        if (test.checkCompleted()) and  (playing==False):
+            playEnd()
+            playing=True
 
-    while correctCombinations < wires:
-        correctCombinations = 0
-        setInputGpios()
-        for pair in combinations:
-            checkPairStatus(pair)
-
-    time.sleep(0.2)
-    playEnd()
     # TODO:Only play once. Wait for a button pressed
     GPIO.cleanup()
 
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
