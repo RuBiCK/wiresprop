aa7b414041f21127b0a2fa3c0c13319dd2928bdb
Rubén Fernández <rubick@gmail.com>
2017-10-15 16:13:31 +0200

    checkpoint

--- wires.py
+++ wires.py
@@ -1,3 +1,4 @@
+import json
 import time
 import RPi.GPIO as GPIO
 import sys
@@ -7,25 +8,29 @@ class Thing(object):
     instances = []
     """Class that will handle a button,selector, switch, etc"""
 
-    def __init__(self, activePort=None, validPort, *passivePorts, **kwargs):
+    def __init__(self, activePort, validPort, *passivePorts, **kwargs):
         """ setup pin modes of the Thing
 
-        'None' means that there is no active port because it takes +5v directly and has no 
+        'None' active port, means that there is no active port because it takes +5v directly and has no 
         possibity to mix with other things like connecting wires"""
-        self.activePort = activePort if not activePort else None 
+        self.activePort = activePort if activePort else None 
         self.validPort = validPort
         self.passivePorts = passivePorts
-        for k,v in kwargs.items():
-            self.k = v
 
+        for k,v in kwargs.items():
+            setattr(self,k,v)
         Thing.instances.append(self)
 
         # input ports are the ones that could receive voltaje that is valid plus passive ports
         self.inputPorts = list(passivePorts)
         self.inputPorts.append(validPort)
 
-        setGpio(inputPorts, "input")
-        setGpio(activePort, "output") if not self.activePort else None
+        setGpio(self.inputPorts, "input")
+        setGpio(self.activePort, "output") if self.activePort else None
+
+
+    def __str__(self):
+            return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def checkCompleted(self):
         """if we activate the activeport and we receive in the validport, the Thing is solved
@@ -33,18 +38,18 @@ class Thing(object):
         must be checked each time you want to know 
         the overall status of the puzzle"""
 
-        GPIO.output(self.activePort, True) if not self.activePort else None
+        GPIO.output(self.activePort, True) if self.activePort else None
         # TODO: Change to interrupts
         if GPIO.input(self.validPort):        
             return True
         else:
             return False
-        GPIO.output(self.activePort, False) if not self.activePort else None
+        GPIO.output(self.activePort, False) if self.activePort else None
         
 
     def checkReset(self):
-       """Test if the Thing is in its normal(reset) state""" 
-        GPIO.output(self.activePort, True)  if not self.activePort else None
+        """ Test if the Thing is in its normal(reset) state """ 
+        GPIO.output(self.activePort, True)  if self.activePort else None
         for port in self.inputPorts:
             if GPIO.input(port):
                 return False
@@ -53,16 +58,16 @@ class Thing(object):
 
 
 
-class WiresPuzzle(Object):
+class WiresPuzzle():
     """handle a wires puzzle composed for various Things"""
     """This puzzle could be something like a pairs of female-female connectors plus more 
     connectors dummies (connected as passive ports) and pairs of wires male-male"""
     ##TODO: Eliminate append instance
-    def __init__(self, steps, **kwargs):
+    def __init__(self, *steps, **kwargs):
         """ steps is a tuple that contain Things"""
         self.steps = steps
         for k,v in kwargs.items():
-            self.k = v
+            setattr(self,k,v)
 
     def __len__(self):
         return len(self.steps)
@@ -91,6 +96,7 @@ class Game:
 
 def setGpio(gpiolist, io):
     """ set a gpio or list of them as input or output mode """
+    print(gpiolist)
     if io == "input":
         GPIO.setup(gpiolist, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     elif io == "output":
@@ -109,11 +115,18 @@ def checkReset(gpiosUsed):
 def main():
     playingMovie = False
     while True:
+        print('\n')
+        currentCheck = ''
         for step in ( step for step in game.secuence if game.secuence.index(step) < game.currentStep ):
-        """ iterate for each game step until the current one """
-            print "Current Puzzle: " + step.name
-        playEnd()
+            """ iterate for each game step until the current one """
+            if not step.checkCompleted():
+                currentCheck = False
+            print("Current Puzzle: " , step.name,'| Realizado: ', currentCheck, ' secuenceindex:',game.secuence.index(step),'checknow:',step.checkCompleted())
+        
         time.sleep(0.2)
+        
+        #playEnd()
+        game.currentStep +=1	
 
     # TODO:Only play once. Wait for a button pressed
 
@@ -124,13 +137,14 @@ if __name__ == "__main__":
     GPIO.setmode(GPIO.BOARD)
     movie=("/data/precuelaGotham.mov")
 
-    seleccionador[0] = Thing(38, 40, 36, 32,{'name': 'seleccionador'})
-    wire[0] = Thing(35,37,{'name': 'corriente'})
-    wire[1] = Thing(31,33,{'name': 'communicaciones'})
-    wire[2] = Thing(21,23,{'name': 'interconexion'})
-    wiresPuzzle = WiresPuzzle(wire[0],wire[1],wire[2],{'name': 'conexiones'})
-    interruptor[0] = Thing(,40, { 'name': 'encendido'})
-    game = Game()
-    game.secuence = [ seleccionador[0], wiresPuzzle, interruptor[0] ]
+    seleccionador = Thing(38, 40, 36, 32, name = 'seleccionador')
+    wire1 = Thing(35,37,name= 'corriente')
+    wire0 = Thing(31,33,name= 'communicaciones')
+    wire2 = Thing(21,23,name= 'interconexion')
+    wiresPuzzle = WiresPuzzle(wire0,wire1,wire2,name = 'conexiones')
+    interruptor = Thing(None,40, name = 'encendido')
+    wire3 = Thing(3,5, name = 'cablesuelto')
+    gameSecuence = [ wire3, seleccionador, wiresPuzzle, interruptor ]
+    game = Game(gameSecuence)
 
     main()
